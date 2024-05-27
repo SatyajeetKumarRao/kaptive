d77d1c7240b24790d990a4e740712db69149d14b
Satyajeet Kumar Rao <satyajeetrao.rao2@gmail.com>
2024-05-28 02:11:43 +0530

    added present view in cashflow table

--- src/components/CashflowSummary.jsx
+++ src/components/CashflowSummary.jsx
@@ -9,8 +9,36 @@ import {
   Thead,
   Tr,
 } from "@chakra-ui/react";
+import { useState } from "react";
+
+import { Select } from "@chakra-ui/react";
 
 const CashflowSummary = ({ data = [] }) => {
+  const [view, setView] = useState("decimal");
+
+  const [currency, setCurrency] = useState("rupees");
+
+  const calculatePercentages = (obj) => {
+    // Calculate the sum of all values in the object
+    const sum = Object.values(obj).reduce((acc, value) => {
+      if (!isNaN(value)) {
+        return acc + value;
+      } else return acc;
+    }, 0);
+
+    // Calculate the percentage for each key
+    const percentages = {};
+    for (const key of Object.keys(obj)) {
+      percentages[key] = !isNaN(obj[key])
+        ? ((obj[key] / sum) * 100).toFixed(2)
+        : obj[key]; // toFixed(2) for 2 decimal places
+    }
+
+    return percentages;
+  };
+
+  console.log(data.map(calculatePercentages));
+
   const monthNames = [
     "January",
     "February",
@@ -42,8 +70,36 @@ const CashflowSummary = ({ data = [] }) => {
         </Box>
 
         <Flex gap={2} flexWrap={"wrap"}>
-          <Button>Decimal View</Button>
-          <Button>Percentage View</Button>
+          <Button
+            size="md"
+            bgColor={view === "decimal" ? "#BEE3F8" : ""}
+            onClick={() => {
+              setView("decimal");
+            }}
+          >
+            Decimal View
+          </Button>
+          <Button
+            size="md"
+            bgColor={view === "percent" ? "#BEE3F8" : ""}
+            onClick={() => {
+              setView("percent");
+            }}
+          >
+            Percentage View
+          </Button>
+
+          <Select
+            width={"fit-content"}
+            value={currency}
+            onChange={(e) => {
+              setCurrency(e.target.value);
+            }}
+          >
+            <option value="rupees">Rupees</option>
+            <option value="dollar">Dollar</option>
+            <option value="euro">Euro</option>
+          </Select>
         </Flex>
       </Flex>
       <Box
@@ -56,7 +112,7 @@ const CashflowSummary = ({ data = [] }) => {
       >
         {/* <TableContainer> */}
         <Table size={{ base: "sm", lg: "md" }}>
-          <TableCaption>{""}</TableCaption>
+          {/* <TableCaption>{""}</TableCaption> */}
           <Thead position="sticky" top="0" bg="gray.100" zIndex="docked">
             <Tr>
               <Th maxW={"200px"} minW={"200px"} whiteSpace={"wrap"}>
@@ -68,24 +124,51 @@ const CashflowSummary = ({ data = [] }) => {
             </Tr>
           </Thead>
           <Tbody>
-            {data.map((item, i) => {
-              return (
-                <Tr key={item.Overhead + i}>
-                  {Object.keys(item).map((key) => {
-                    return (
-                      <Td
-                        key={key}
-                        maxW={i === 0 ? "200px" : ""}
-                        minW={i === 0 ? "200px" : ""}
-                        whiteSpace={"wrap"}
-                      >
-                        {item[key]}
-                      </Td>
-                    );
-                  })}
-                </Tr>
-              );
-            })}
+            {view === "decimal" &&
+              data.map((item, index) => {
+                return (
+                  <Tr key={item.Overhead + index}>
+                    {Object.keys(item).map((key, i) => {
+                      return (
+                        <Td
+                          key={key}
+                          maxW={i === 0 ? "200px" : "150px"}
+                          minW={i === 0 ? "200px" : "150px"}
+                          whiteSpace={"wrap"}
+                        >
+                          {isNaN(item[key])
+                            ? item[key]
+                            : currency == "rupees"
+                            ? Math.floor(item[key])
+                            : currency == "dollar"
+                            ? Math.floor(item[key] / 83.15)
+                            : Math.floor(item[key] / 90.28)}
+                        </Td>
+                      );
+                    })}
+                  </Tr>
+                );
+              })}
+
+            {view === "percent" &&
+              data.map(calculatePercentages).map((item, index) => {
+                return (
+                  <Tr key={item.Overhead + index}>
+                    {Object.keys(item).map((key, i) => {
+                      return (
+                        <Td
+                          key={key}
+                          maxW={i === 0 ? "200px" : "100px"}
+                          minW={i === 0 ? "200px" : "100px"}
+                          whiteSpace={"wrap"}
+                        >
+                          {isNaN(item[key]) ? item[key] : item[key] + "%"}
+                        </Td>
+                      );
+                    })}
+                  </Tr>
+                );
+              })}
           </Tbody>
         </Table>
         {/* </TableContainer> */}
